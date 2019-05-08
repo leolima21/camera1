1cef5571a94a125ba85100aa01a2d8b637d9e3bd
leolima21 <50239670+leolima21@users.noreply.github.com>
2019-05-08 17:10:20 -0300

    Update camera_sub.py

--- camera_sub.py
+++ camera_sub.py
@@ -4,6 +4,7 @@
 import rospy
 import cv2
 import time
+import cv_bridge
 import numpy as np
 from std_msgs.msg import String
 from std_msgs.msg import Int32
@@ -13,10 +14,16 @@ from sensor_msgs.msg import Image
 # Funcao q ira executar o movimento do robo
 def callback(data):
     # Criação do objeto da imagem
-    ret, frame = data.data
+    ros_ret, ros_frame = data.data
+
+    # Criacao do objeto de convercao
+    #bridge = CvBridge()
+
+    # Converter a imagem ros para imagem cv2
+    cv2_ret, cv2_frame = bridge.imgmsg_to_cv2(ros_frame, "bgr8")
 
     # Exibir a imagem em uma janela
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', cv2_frame)
 
     # Fechar a janela pressionando a tecla 'q'
 	if cv2.waitKey(1) == ord('q'):
